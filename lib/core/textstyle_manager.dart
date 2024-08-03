e6d7b9855d725238a0f640a38f1a82057a5e5681
webs127 <davidshittu20@gmail.com>
2024-08-02 18:59:00 -0700

    Textstyle manager

--- lib/core/textstyle_manager.dart
+++ lib/core/textstyle_manager.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-TextStyle _textStyle(String fontFamily, double fontSize, Color color, FontWeight fontWeight) => TextStyle(
-  fontFamily: fontFamily,
-  fontSize: fontSize,
-  color: color,
-  fontWeight: fontWeight,
-);
+TextStyle _textStyle(String fontFamily, double fontSize, Color color,
+        FontWeight fontWeight, FontStyle fontStyle) =>
+    TextStyle(
+      fontFamily: fontFamily,
+      fontSize: fontSize,
+      color: color,
+      fontWeight: fontWeight,
+      fontStyle: fontStyle,
+    );
 
-TextStyle regularTextStyle({String fontFamily = "Poppins", required double fontSize, Color color =  Colors.black, FontWeight fontWeight = FontWeight.w400}) => _textStyle(fontFamily, fontSize, color, fontWeight);
+TextStyle regularTextStyle(
+        {String fontFamily = "Segoe UI",
+        required double fontSize,
+        Color color = Colors.black,
+        FontStyle fontStyle = FontStyle.normal,
+        FontWeight fontWeight = FontWeight.w400}) =>
+    _textStyle(fontFamily, fontSize, color, fontWeight, fontStyle);
 
-TextStyle mediumTextStyle({String fontFamily = "Poppins", required double fontSize, Color color =  Colors.black, FontWeight fontWeight = FontWeight.w500}) => _textStyle(fontFamily, fontSize, color, fontWeight);
+TextStyle mediumTextStyle(
+        {String fontFamily = "Segoe UI",
+        required double fontSize,
+        Color color = Colors.black,
+        FontStyle fontStyle = FontStyle.normal,
+        FontWeight fontWeight = FontWeight.w500}) =>
+    _textStyle(fontFamily, fontSize, color, fontWeight, fontStyle);
 
-TextStyle blackTextStyle({String fontFamily = "Poppins", required double fontSize, Color color =  Colors.black, FontWeight fontWeight = FontWeight.w900}) => _textStyle(fontFamily, fontSize, color, fontWeight);
+TextStyle blackTextStyle({
+  String fontFamily = "Segoe UI",
+  required double fontSize,
+  Color color = Colors.black,
+  FontStyle fontStyle = FontStyle.normal,
+  FontWeight fontWeight = FontWeight.w900,
+}) =>
+    _textStyle(fontFamily, fontSize, color, fontWeight, fontStyle);
+
+TextStyle italicTextStyle(
+        {String fontFamily = "Segoe UI",
+        FontStyle fontStyle = FontStyle.normal,
+        required double fontSize,
+        Color color = Colors.black,
+        FontWeight fontWeight = FontWeight.w900}) =>
+    _textStyle(fontFamily, fontSize, color, fontWeight, fontStyle);
