712af60d79f2e7387a99671c9ca127f1d67d70b3
webs127 <davidshittu20@gmail.com>
2024-12-02 00:05:50 +0100

    Minor changes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,21 +1,23 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
-import 'package:spenum/app/route.dart';
-import 'package:spenum/services/random_number_generator_viewmodel.dart';
+import 'package:spenum/app/index.dart';
 
-class MyApp extends StatelessWidget {
-   const MyApp._internal();
+class MyApp extends StatefulWidget {
+  const MyApp._internal();
   static MyApp instance = const MyApp._internal();
   factory MyApp() => instance;
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver{
   @override
   Widget build(BuildContext context) {
     return  ScreenUtilInit(
       designSize: const Size(375,812),
       child: MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => RandomNumberGeneratorViewmodel())
+          ChangeNotifierProvider(create: (_) => RandomNumberGeneratorViewmodel()),
+          ChangeNotifierProvider(create: (_) => SettingsViewmodel()),
         ],
         child: const MaterialApp(
           initialRoute: RouteManager.splash,
